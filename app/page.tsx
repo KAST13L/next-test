e6cba6b78ce87b18f5c7ce2c291e23ce2c84e2a1
KAST13L <o4enbsmeshno@gmail.com>
2023-05-25 16:22:30 +0300

    feat: add rtk and rtk query to next app

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <div
           style={{
             display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
             gap: 20,
           }}
         >
