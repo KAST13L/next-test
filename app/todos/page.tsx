dcd8a90107840a27f0fb9642f4b00045940c0599
KAST13L <o4enbsmeshno@gmail.com>
2023-05-25 13:51:54 +0300

    feat: create todos page

---
+++ app/todos/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Todos | Next JS App",
+};
+
+export default function Todos() {
+  return (
+    <>
+      <h1>Todos</h1>
+    </>
+  );
+}
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,6 +8,7 @@ export const Header = () => {
         <Link href={"/"}>Home</Link>
         <Link href={"/about"}>About</Link>
         <Link href={"/blog"}>Blog</Link>
+        <Link href={"/todos"}>Todos</Link>
       </div>
     </header>
   );
